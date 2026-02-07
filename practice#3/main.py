7d934249067db1ce5cfa9df5511066869e95fdb8
JunSeo <cracker1010987@gmail.com>
2026-02-07 14:52:21 +0900

    키오스크

--- practice#3/main.py
+++ practice#3/main.py
@@ -7,14 +7,15 @@ order_price = menu.get_price(order)
  
 if order_price is None:
     print("없는 메뉴입니다.")
-
-print(f"[{order}]를 선택하셨습니다. 가격은 {order_price}원 입니다.")
-
-money = int(input("돈을 넣어주세요: "))
-
-result = payment.pay(order_price, money)
-
-if result >= 0:
-    print(f"결제 완료! 거스름돈 {result}원을 받아가세요.")
+    
 else:
-    print("잔액이 부족합니다. 결제가 취소됩니다.")
+    print(f"[{order}]를 선택하셨습니다. 가격은 {order_price}원 입니다.")
+
+    money = int(input("돈을 넣어주세요: "))
+
+    result = payment.pay(order_price, money)
+
+    if result >= 0:
+        print(f"결제 완료! 거스름돈 {result}원을 받아가세요.")
+    else:
+        print("잔액이 부족합니다. 결제가 취소됩니다.")
